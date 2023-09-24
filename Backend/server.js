cc186ed6d3524b15917f6431954f590b06d80cce
Inzamam Yousaf <80321391+Inzamam1121@users.noreply.github.com>
2023-09-24 18:03:15 +0500

    u

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const app = express();
+const axios = require('axios');
 const port = process.env.PORT || 5000;
 
-// Define a route that responds with "Welcome to Archikraft Backend Server"
 app.get('/', (req, res) => {
   res.send('Welcome to Archikraft Backend Server');
+});
+
+app.get('/saadapi', async (req, res) => {
+  try {
+    const apiUrl = 'https://d82a-103-77-11-146.ngrok-free.app/';
+    const response = await axios.get(apiUrl);
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: 'An error occurred' });
+  }
 });
 
 // Start the server
